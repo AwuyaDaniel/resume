fdf2d0d58e2badc5778745cd9fc21c0e49237c01
freezy <awuyadanielz@gmail.com>
2021-04-10 08:48:36 +0100

    First commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn awuyadaniel.wsgi --log-file -
---
+++ resume/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+
+# Create your views here.
+
+
+def index(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        context = {'name': name}
+        #
+        # # Send an Email
+        # send_mail(
+        #     'from: ' + name + "About: " + subject,  # Subject
+        #     message,  # Message
+        #     email,  # From mail
+        #     ['awuyadanielz@gmail.com'],  # To Mail
+        # )
+
+        return render(request, 'resume/index.html', context)
+    else:
+        return render(request, 'resume/index.html')
