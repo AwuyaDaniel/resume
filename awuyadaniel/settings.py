1e4a944c803cfeb4156cd76d40fba8d13d65e161
Daniel <47531721+AwuyaDaniel@users.noreply.github.com>
2023-12-29 22:06:10 +0100

    reconstruction2

--- awuyadaniel/settings.py
+++ awuyadaniel/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,8 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
-
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # Heroku Settings
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
